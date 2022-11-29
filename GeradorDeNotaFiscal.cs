c770981dc4abf5617c9599039c222745e57c3c63
RIcardo Souza <ryszard696@yahoo.com.br>
2022-11-29 09:44:19 -0300

    finalizado, porém os testes não passam

--- GeradorDeNotaFiscal.cs
+++ GeradorDeNotaFiscal.cs
@@ -9,12 +9,14 @@ namespace TDDNotaFiscal
 {
     public class GeradorDeNotaFiscal
     {
+        private IList<IAcaoAposGerarNota> Acoes;
+        private IRelogio Relogio;
+        private ITabela Tabela;
         private NFDao dao;
         private SAP sap;
 
         public GeradorDeNotaFiscal()
-        {
-        }
+        { }
 
         public GeradorDeNotaFiscal(NFDao _dao)
         {
@@ -27,9 +29,39 @@ namespace TDDNotaFiscal
             this.sap = _sap;
         }
 
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes, IRelogio relogio)
+        {
+            this.Acoes = acoes;
+            this.Relogio = relogio;
+        }
+
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes, ITabela tabela)
+        {
+            this.Acoes = acoes;
+            this.Tabela = tabela;
+        }
+
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes, IRelogio relogio, ITabela tabela)
+        {
+            this.Acoes = acoes;
+            this.Relogio = relogio;
+            this.Tabela = tabela;
+        }
+
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes) : this(acoes, new RelogioDoSistema())
+        { }
+
         public NotaFiscal GerarNota(Pedido pedido)
         {
-            NotaFiscal nf = new NotaFiscal(pedido.Cliente, pedido.ValorTotal * 0.94, DateTime.Now);
+            NotaFiscal nf = new NotaFiscal(pedido.Cliente, pedido.ValorTotal * Tabela.ParaValor(pedido.ValorTotal), Relogio.Hoje());
+
+            if (Acoes != null)
+            {
+                foreach (var acao in Acoes)
+                {
+                    acao.Executa(nf);
+                }
+            }
 
             //new NFDao().Persiste(nf);
 
---
+++ IAcaoAposGerarNota.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDNotaFiscal.Model;
+
+namespace TDDNotaFiscal
+{
+    public interface IAcaoAposGerarNota
+    {
+        void Executa(NotaFiscal nf);
+    }
+}
